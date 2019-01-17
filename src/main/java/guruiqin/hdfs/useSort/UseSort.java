19ebd76e999fcd1d3f2f48728c8a8316655b1392
ruiqingu <ruiqingu@tencent.com>
2019-01-17 14:37:07 +0800

    add

--- src/main/java/guruiqin/hdfs/useSort/UseSort.java
+++ src/main/java/guruiqin/hdfs/useSort/UseSort.java
@@ -2,17 +2,23 @@ package guruiqin.hdfs.useSort;
 
 import guruiqin.hdfs.logAnalysis.LogBean;
 import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class UseSort {
 
     public static class SortMapper extends Mapper<LongWritable, Text, LogBean, NullWritable>{
+
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
@@ -36,7 +42,26 @@ public class UseSort {
         }
     }
 
-    public static void main(String []args){
+    public static void main(String []args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(UseSort.class);
+
+
+        job.setMapOutputKeyClass(LogBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        //设置reducer 输出 k-v 格式
+        job.setOutputKeyClass(LogBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        //指定input 文件路径
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        job.waitForCompletion(true);
 
     }
 
